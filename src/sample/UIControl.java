1a94a09194c61b7ecd496ee2df4a948a42ad9366
miledevv <miledevv@gmail.com>
2020-08-07 15:24:53 +0200

    Initial commit

---
+++ src/sample/UIControl.java
@@ -0,0 +1,17 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class UIControl {
+
+    @FXML
+    private Button locationBttn;
+
+    @FXML
+    void locationButton(ActionEvent event) {
+
+    }
+}
