13c35ec63fad73d53287477d3f781c5e8d31d295
miledevv <miledevv@gmail.com>
2020-08-08 11:29:12 +0200

    Login screen & Homepage partially done

---
+++ src/sample/HomepageUI.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class HomepageUI {
+
+    @FXML
+    void heartPressed(MouseEvent event) {
+                System.out.println("PRESSED");
+    }
+
+}
+
+
+
--- src/sample/LocationUI.java
+++ src/sample/LocationUI.java
@@ -2,8 +2,15 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LocationUI {
 
@@ -14,5 +21,16 @@ public class LocationUI {
     @FXML
     void exitButton(ActionEvent event) {
             System.exit(0);
+    }
+
+    @FXML
+    void goToMainScreen(ActionEvent event) throws IOException {
+        Parent homepage = FXMLLoader.load(getClass().getResource("HomepageUI.fxml"));
+        Scene homepageScene = new Scene(homepage);
+
+        //This line gets stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(homepageScene);
+        window.show();
     }
 }
