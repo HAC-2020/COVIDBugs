d92dc660702975e7841e418409d138ed846baccd
william <wlphyo@ucsc.edu>
2020-08-07 19:00:23 -0700

    second commit

---
+++ Shop/Shop.java
@@ -0,0 +1,21 @@
+package Shop;
+import java.util.*;
+
+public class Shop {
+    public static double distance(double lat1,double lat2,double lon1,double lon2){
+        lat1= Math.toRadians(lat1);
+        lat2= Math.toRadians(lat2);
+        lon1= Math.toRadians(lon1);
+        lon2= Math.toRadians(lon2);
+
+        /*Haversine formula*/
+        double dlat = lat2 - lat1;
+        double dlon = lon2 - lon1;
+        double a = Math.pow(Math.sin(dlat/2),2)+
+                Math.cos(lat1)*Math.cos(lat2)+
+                Math.pow(Math.sin(dlon/2),2);
+        double c = 2*Math.asin(Math.sqrt(a));
+        double r = 3956;   //this is in Km, use 3956 for mi
+        return c * r;
+    }
+}
