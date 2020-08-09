7461b6d929649557f8ce171151aded51f838956a
Erin Cho <64560658+erin-cho@users.noreply.github.com>
2020-08-09 04:51:15 -0400

    Update Buyer.java

--- Buyer/Buyer.java
+++ Buyer/Buyer.java
@@ -5,11 +5,14 @@ import Item.Item;
 public class Buyer {
 	private String location;
 	private LinkedList<Item> cart;
-	public int size = 0;
+	private LinkedList<Item> saved;
+	public int sizeCart = 0;
+	public int sizeSaved = 0;
 	
 	public Buyer() {
 		location = "";
 		cart = new LinkedList<Item>();
+		saved = new LinkedList<Item>();
 	}
 	
 	public void chooseCity(String city) {
@@ -19,30 +22,54 @@ public class Buyer {
 	public void addToCart(Item product) {
 		cart.add(product);
 		System.out.println(product.getDescription() + " has been added to cart!");
-		size++;
+		sizeCart++;
 
 	}
 	
 	public void removeFromCart(Item product) {
 		cart.remove(product);
 		System.out.println(product.getDescription() + " has been removed from cart.");
-		size--;
+		sizeCart--;
 	}
 	
-//	@Override
-//	public String toString() {
-//		return cart.;
-//	}
+	public void addToSaved(Item product) {
+		saved.add(product);
+		System.out.println(product.getDescription() + " has been added to your saved items!");
+		sizeSaved++;
+	}
 	
-	//override linkedlist toString
+	public void removeFromSaved(Item product) {
+		saved.remove(product);
+		System.out.println(product.getDescription() + " has been removed from your saved items.");
+		sizeSaved--;
+	}
+	
+	public void moveFromCartToSaved(Item product) {
+		if (cart.contains(product)) {
+			cart.remove(product);
+			sizeCart--;
+			saved.add(product);
+			sizeSaved++;
+			System.out.println(product.getDescription() + " has been moved to your saved items!");
+		}
+	}
+	
 	public void viewCart() {
 		System.out.print("[");
-		for(int i = 0; i < cart.size();i++){
+		for(int i = 0; i < cart.sizeCart();i++){
 				System.out.print(cart.get(i).getDescription());
-				if(i == cart.size()-1) System.out.println("]");
+				if(i == cart.sizeCart()-1) System.out.println("]");
 				else System.out.print(", ");
 		}
-
+	}
+	
+	public void viewSaved() {
+		System.out.print("[");
+		for(int i = 0; i < saved.sizeSaved();i++){
+				System.out.print(saved.get(i).getDescription());
+				if(i == saved.sizeSaved()-1) System.out.println("]");
+				else System.out.print(", ");
+		}
 	}
 	
 }
