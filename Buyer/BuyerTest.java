551e7e69255ba06973590809ede98eed8713a5f4
miledevv <miledevv@gmail.com>
2020-08-09 11:32:22 +0200

    Pushing all together

--- Buyer/BuyerTest.java
+++ Buyer/BuyerTest.java
@@ -12,12 +12,22 @@ public class BuyerTest {
 		Item mask = new Item(1234, 1, "Mask", 1.99);
 		Item tp = new Item(1235, 1, "Toilet paper", 3.00);
 		Item sanitizer = new Item(1223, 1, "Hand sanitizer", 5.00);
+		Item wipes = new Item(1333, 1, "Wipes", 1.00);
 		
 		buyer1.addToCart(mask);
 		buyer1.addToCart(tp);
 		buyer1.addToCart(sanitizer);
 		buyer1.removeFromCart(tp);
+		
 		buyer1.viewCart();
+
+		buyer1.addToSaved(tp);
+		buyer1.addToSaved(wipes);
+		buyer1.moveFromCartToSaved(mask);
+		
+		buyer1.viewCart();
+		buyer1.viewSaved();
+
 		
 	}
 }
